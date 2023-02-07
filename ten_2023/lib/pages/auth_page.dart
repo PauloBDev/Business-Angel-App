4454fc2df7f26e9ab14fd457387d1f67bea86a49
PauloB <paulo.bernardino2@gmail.com>
2023-02-07 12:10:12 +0000

    paulo pc

--- ten_2023/lib/pages/auth_page.dart
+++ ten_2023/lib/pages/auth_page.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tg2/pages/home_page.dart';
-import 'package:tg2/pages/login_screen.dart';
+
+import 'home_page.dart';
+import 'login_screen.dart';
 
 class AuthPage extends StatelessWidget {
   static final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
--- ten_2023/lib/pages/home_page.dart
+++ ten_2023/lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tg2/pages/auth_page.dart';
+
+import 'auth_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
