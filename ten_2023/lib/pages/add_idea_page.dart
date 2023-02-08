247eb2f7f6eca973dfcb6da24b5485bdaf245f78
telmoss7 <telmo_escapaes@hotmail.com>
2023-02-08 16:59:31 +0000

    Merge branch 'main' of https://github.com/PauloBDev/TEN_2022

--- ten_2023/lib/pages/add_idea_page.dart
+++ ten_2023/lib/pages/add_idea_page.dart
@@ -7,8 +7,12 @@
 // paulo / joão
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+
+import 'dart:typed_data';
+import 'auth_page.dart';
 
 class IdeaAddPage extends StatefulWidget {
   const IdeaAddPage({super.key});
@@ -23,6 +27,10 @@ const List<String> dropdownlist = <String>[
 ];
 
 class _IdeaAddPageState extends State<IdeaAddPage> {
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   String dropdownvalue = dropdownlist.first;
   final List<TextEditingController> _controllers =
       List.generate(2, (i) => TextEditingController());
@@ -67,10 +75,12 @@ class _IdeaAddPageState extends State<IdeaAddPage> {
             double progress = data.bytesTransferred / data.totalBytes;
             return SizedBox(
               height: 50,
+              width: 50,
               child: Stack(
                 fit: StackFit.expand,
                 children: [
                   CircularProgressIndicator(
+                    strokeWidth: 20,
                     value: progress,
                     backgroundColor: Colors.grey,
                     color: Colors.black,
@@ -80,6 +90,7 @@ class _IdeaAddPageState extends State<IdeaAddPage> {
                       "${(100 * progress).roundToDouble()}%",
                       style: const TextStyle(
                         color: Colors.black,
+                        backgroundColor: Colors.white,
                       ),
                     ),
                   )
@@ -96,153 +107,349 @@ class _IdeaAddPageState extends State<IdeaAddPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (pickedFile != null)
-              Expanded(
-                child: SafeArea(
-                  child: Container(
-                    color: Colors.grey,
+        appBar: AppBar(
+          title: const Text('Add idea'),
+          actions: [
+            Container(
+              padding: const EdgeInsets.all(3),
+              decoration: BoxDecoration(
+                  color: Colors.black87,
+                  borderRadius: BorderRadius.circular(30)),
+              child: IconButton(
+                  onPressed: () {
+                    signOut();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: ((context) {
+                          return const AuthPage();
+                        }),
+                      ),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Colors.white,
+                  )),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 5,
+                ),
+                if (pickedFile != null)
+                  SafeArea(
                     child: Center(
-                      child: Text(
-                        pickedFile!.name,
+                      child: Image.file(
+                        File(pickedFile!.path!),
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 200,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                        ),
+                        onPressed: selectFile,
+                        child: const Text(
+                          "Selecione Ficheiro",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 50,
+                        width: 200,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                          ),
+                          onPressed: uploadFile,
+                          child: const Text(
+                            "Upload Ficheiro",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    buildProgress(),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  controller: _controllers[0],
+                  decoration: InputDecoration(
+                    labelText: 'Titulo',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: const OutlineInputBorder(),
+                    suffix: IconButton(
+                      onPressed: () {
+                        _controllers[0].clear();
+                      },
+                      icon: const Icon(
+                        Icons.clear,
                       ),
                     ),
                   ),
                 ),
-              ),
-            const SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              height: 50,
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
+                const SizedBox(
+                  height: 15,
                 ),
-                onPressed: selectFile,
-                child: const Text(
-                  "Selecione Ficheiro",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              height: 50,
-              width: 200,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                ),
-                onPressed: uploadFile,
-                child: const Text(
-                  "Upload Ficheiro",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            buildProgress(),
-            TextField(
-              controller: _controllers[0],
-              decoration: InputDecoration(
-                labelText: 'Titulo',
-                filled: true,
-                fillColor: Colors.white,
-                border: const OutlineInputBorder(),
-                suffix: IconButton(
-                  onPressed: () {
-                    _controllers[0].clear();
-                  },
-                  icon: const Icon(
-                    Icons.clear,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            TextField(
-              controller: _controllers[1],
-              decoration: InputDecoration(
-                labelText: 'Descrição',
-                filled: true,
-                fillColor: Colors.white,
-                border: const OutlineInputBorder(),
-                suffix: IconButton(
-                  onPressed: () {
-                    _controllers[1].clear();
-                  },
-                  icon: const Icon(
-                    Icons.clear,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Align(
-              alignment: const Alignment(
-                -1,
-                1,
-              ),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: DropdownButton<String>(
-                  value: dropdownvalue,
-                  style: const TextStyle(
-                    color: Colors.white,
-                  ),
-                  icon: const Icon(
-                    Icons.expand_more,
-                    color: Colors.blue,
-                  ),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.black,
-                  ),
-                  onChanged: (String? value) {
-                    setState(() {
-                      dropdownvalue = value!;
-                    });
-                  },
-                  items: dropdownlist
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: const TextStyle(
-                          color: Colors.black,
-                        ),
+                TextField(
+                  controller: _controllers[1],
+                  decoration: InputDecoration(
+                    labelText: 'Descrição',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: const OutlineInputBorder(),
+                    suffix: IconButton(
+                      onPressed: () {
+                        _controllers[1].clear();
+                      },
+                      icon: const Icon(
+                        Icons.clear,
                       ),
-                    );
-                  }).toList(),
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Align(
+                  alignment: const Alignment(
+                    -1,
+                    1,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: DropdownButton<String>(
+                      value: dropdownvalue,
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      icon: const Icon(
+                        Icons.expand_more,
+                        color: Colors.blue,
+                      ),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.black,
+                      ),
+                      onChanged: (String? value) {
+                        setState(() {
+                          dropdownvalue = value!;
+                        });
+                      },
+                      items: dropdownlist
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(
+                            value,
+                            style: const TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        )
+        // Padding(
+        //   padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       if (pickedFile != null)
+        //         Expanded(
+        //           child: SafeArea(
+        //             child: Center(
+        //               child: Image.file(
+        //                 File(pickedFile!.path!),
+        //                 width: double.infinity,
+        //                 fit: BoxFit.cover,
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       const SizedBox(
+        //         height: 15,
+        //       ),
+        //       SizedBox(
+        //         height: 50,
+        //         width: 200,
+        //         child: ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             backgroundColor: Colors.black,
+        //           ),
+        //           onPressed: selectFile,
+        //           child: const Text(
+        //             "Selecione Ficheiro",
+        //             style: TextStyle(
+        //               color: Colors.white,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //       const SizedBox(
+        //         height: 20,
+        //       ),
+        //       SizedBox(
+        //         height: 50,
+        //         width: 200,
+        //         child: ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             backgroundColor: Colors.black,
+        //           ),
+        //           onPressed: uploadFile,
+        //           child: const Text(
+        //             "Upload Ficheiro",
+        //             style: TextStyle(
+        //               color: Colors.white,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //       const SizedBox(
+        //         height: 15,
+        //       ),
+        //       buildProgress(),
+        //       const SizedBox(
+        //         height: 15,
+        //       ),
+        //       TextField(
+        //         controller: _controllers[0],
+        //         decoration: InputDecoration(
+        //           labelText: 'Titulo',
+        //           filled: true,
+        //           fillColor: Colors.white,
+        //           border: const OutlineInputBorder(),
+        //           suffix: IconButton(
+        //             onPressed: () {
+        //               _controllers[0].clear();
+        //             },
+        //             icon: const Icon(
+        //               Icons.clear,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //       const SizedBox(
+        //         height: 15,
+        //       ),
+        //       TextField(
+        //         controller: _controllers[1],
+        //         decoration: InputDecoration(
+        //           labelText: 'Descrição',
+        //           filled: true,
+        //           fillColor: Colors.white,
+        //           border: const OutlineInputBorder(),
+        //           suffix: IconButton(
+        //             onPressed: () {
+        //               _controllers[1].clear();
+        //             },
+        //             icon: const Icon(
+        //               Icons.clear,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //       const SizedBox(
+        //         height: 15,
+        //       ),
+        //       Align(
+        //         alignment: const Alignment(
+        //           -1,
+        //           1,
+        //         ),
+        //         child: Container(
+        //           padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        //           decoration: BoxDecoration(
+        //             color: Colors.grey,
+        //             borderRadius: BorderRadius.circular(8),
+        //           ),
+        //           child: DropdownButton<String>(
+        //             value: dropdownvalue,
+        //             style: const TextStyle(
+        //               color: Colors.white,
+        //             ),
+        //             icon: const Icon(
+        //               Icons.expand_more,
+        //               color: Colors.blue,
+        //             ),
+        //             underline: Container(
+        //               height: 2,
+        //               color: Colors.black,
+        //             ),
+        //             onChanged: (String? value) {
+        //               setState(() {
+        //                 dropdownvalue = value!;
+        //               });
+        //             },
+        //             items: dropdownlist
+        //                 .map<DropdownMenuItem<String>>((String value) {
+        //               return DropdownMenuItem<String>(
+        //                 value: value,
+        //                 child: Text(
+        //                   value,
+        //                   style: const TextStyle(
+        //                     color: Colors.black,
+        //                   ),
+        //                 ),
+        //               );
+        //             }).toList(),
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        );
   }
 }
